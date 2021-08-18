b7850426971d6f0d88625b4e2b2fb3d5c16a9078
KalkidanTaye <kali.taye@gmail.com>
2021-08-18 01:04:04 +0000

    Fix else statement

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else console.log(`Assertion Failed:  ${actual}  !==  ${actual}`);
+  } else console.log(`Assertion Failed:  ${actual}  !==  ${expected}`);
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
