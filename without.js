3446a5dd65b6d894cf4532532af2e7e7ce6eeb71
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-19 19:01:11 -0400

    a function that retuns an array after removing items

---
+++ without.js
@@ -0,0 +1,35 @@
+const assertArrayEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else console.log(`Assertion Failed:  ${actual}  !==  ${expected}`);
+};
+
+const eqArrays = function (array1, array2) {
+  isMatching = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isMatching = false;
+    }
+  }
+  return isMatching;
+};
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i]);
+    }
+  }
+  // for (let remove in itemsToRemove) {
+  //   console.log(itemsToRemove[remove]);
+  // }
+  return newArr;
+};
+
+const words = ["hello", "world", "lighthouse"];
+assertArrayEqual(
+  eqArrays(without(words, ["hello", "lighthouse"]), ["world"]),
+  true
+);
