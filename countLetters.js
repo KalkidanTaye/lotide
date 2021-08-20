e2fc8314a33ae4fe29485693b1fab66202fec2d1
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-20 00:24:14 -0400

    A function that return how many letters are there in a given array

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else console.log(`Assertion Failed:  ${actual}  !==  ${expected}`);
+};
+
+const countLetters = function (countWord) {
+  const results = {};
+
+  for (const letter of countWord) {
+    if (letter === " ") {
+      continue;
+    } else if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+
+  return results;
+};
+const resultLetters = countLetters("lighthouse in the house");
+assertEqual(resultLetters["h"], 4);
