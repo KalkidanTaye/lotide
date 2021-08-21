90e49c863df2a0db820210dd612dcebdd47f48db
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-21 04:08:20 -0400

    A Function prints all the array until some condition passed

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const assertArrayEqual = function (actual, expected) {
+  const isEqual = eqArrays(actual, expected);
+  if (isEqual) {
+    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else console.log(`Assertion Failed:  ${actual}  !==  ${expected}`);
+};
+
+const eqArrays = function (array1, array2) {
+  isMatching = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isMatching = false;
+    }
+  }
+  return isMatching;
+};
+
+const takeUntil = function(array, callback) {
+  const newArr=[]
+    for (const iterator of array) {
+      if(callback(iterator)){
+        break
+      }
+      newArr.push(iterator)  
+    }
+    return newArr
+  }
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArrayEqual(results1, [ 1, 2, 5, 7, 2 ] , true);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArrayEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ], true);
