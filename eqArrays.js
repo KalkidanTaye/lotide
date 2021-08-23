f2c7b6010cb686cddf5e6ddb1f3c91242a946103
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 15:30:43 -0400

    Export module

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else console.log(`Assertion Failed:  ${actual}  !==  ${expected}`);
-};
+const assertEqual = require("./assertEqual");
 
 const eqArrays = function (array1, array2) {
   isMatching = false;
@@ -17,8 +13,4 @@ const eqArrays = function (array1, array2) {
   return isMatching;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+module.exports = eqArrays;
