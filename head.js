f5a2e863515b2acc1f080a25008997a1e9a93c6f
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 14:08:15 -0400

    import assertEqual function

--- head.js
+++ head.js
@@ -1,8 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else console.log(`Assertion Failed:  ${actual}  !==  ${actual}`);
-};
+const assertEqual = require("./assertEqual");
 
 const head = function (actual) {
   return actual[0];
