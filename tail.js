400391489a7afbf9e2f164bd7d01c11fdd26e803
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 14:51:06 -0400

    export module

--- tail.js
+++ tail.js
@@ -1,8 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else console.log(`Assertion Failed:  ${actual}  !==  ${actual}`);
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function (arr) {
   const tailArr = [];
@@ -12,8 +8,4 @@ const tail = function (arr) {
   return tailArr;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
