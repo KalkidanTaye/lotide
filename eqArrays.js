19e9afbd90e3418d1c9cbc0b9e12c952948cbed1
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 23:26:03 -0400

    mocha and chai testing implemented on headTest

--- eqArrays.js
+++ eqArrays.js
@@ -23,8 +23,8 @@ const eqArrays = function (array1, array2) {
 
 module.exports = eqArrays;
 
-assertEqual([1, 2, 3], [1, 2, 3]); // => true
-assertEqual([1, 2, 3], [3, 2, 1]); // => false
+// assertEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+// assertEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+// assertEqual(["1", "2", "3"], ["1", "2", 3]); // => false
