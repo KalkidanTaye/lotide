b0b8396400f91f91a111e4b242d30e352a502127
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 21:34:04 -0400

    a test for assertArraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArrayEqual = require("../assertArraysEqual");
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]); // => trueCLEAR
+
+assertArrayEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArrayEqual(["1", "2", "3"], ["1", "2", 3]); // => false
