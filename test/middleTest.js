5871fa89b9ebada77cfdb1ee6ffc81eadf6cf653
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-23 21:54:14 -0400

    test for middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,8 @@
 const middle = require("../middle");
 const assertArrayEqual = require("../assertArraysEqual");
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2, 3, 4, 5])); // => [2]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [3,4]
+assertArrayEqual(middle([1]), [1]); // => []
+assertArrayEqual(middle([1, 2, 3, 4, 5]), [2]); // => [2]
+assertArrayEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // => [3,4]
 
 assertArrayEqual(middle([1, 2, 3, 4, 5]), [3], true); // => true
