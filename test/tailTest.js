a3daa2ff19f9e29bbdc24e1f300e77d7e5d13d3e
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-24 00:42:42 -0400

    refactor with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,12 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
+
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns 'Labs' for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+  });
+  it("returns '8' for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6, 7, 8]), 8);
+  });
+});
